2dffcef852f8878d26d1ae9d2cde3c9c093876c6
amonecke <amonecke@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-05-06 11:19:06 +0000

    try to fix warning for multiple calls to data() for the objects: ECSImm, ECSIsm, mobi
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sempls@13 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/semPLS/data/ECSImobi.R
+++ pkg/semPLS/data/ECSImobi.R
@@ -1,5 +1,5 @@
-load("mobi.rda")
-load("ECSIsm.rda")
-load("ECSImm.rda")
+load(system.file("mobi.rda", package="semPLS"))
+load(system.file("ECSIsm.rda", package="semPLS"))
+load(system.file("ECSImm.rda", package="semPLS"))
 ECSImobi <- semPLS:::specifyPLSM(data=mobi, strucmod=ECSIsm, measuremod=ECSImm, order="generic")
 
