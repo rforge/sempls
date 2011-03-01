7d2ad19202b05178d64189649cdc9aedcae7f7c5
amonecke <amonecke@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-03-01 12:22:00 +0000

    'predict.R', 'residuals.R' als eigene Dateien
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sempls@65 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/semPLS/R/residuals.R
@@ -0,0 +1,25 @@
+# 01.03.2011
+residuals <- function(object, what=c("LVs", "MVs"), scale=c("original", "scaled"), total=FALSE){
+    what <- match.arg(what)
+    scale <- match.arg(scale)
+    model <- object$model
+    data <- object$data
+    
+    if(what=="LVs"){
+        res <- object$factor_scores - predict(object, what, scale, total)
+    }
+    else{
+      if(scale=="scaled"){
+        pdata <- predict(object, what, scale, total)$mv_prediction
+        res <- data - pdata
+      }
+      else{
+        m <- attr(data, "scaled:center")
+        s <- attr(data, "scaled:scale")
+        data <- t(t(data) * s) + m
+        pdata <- predict(object, what, scale, total)$mv_prediction
+        res <- data - pdata
+      }
+    }
+    return(res)
+}
