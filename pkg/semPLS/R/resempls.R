ce02cafe1e294d211f3781a45c3081e9c695259f
amonecke <amonecke@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-07-14 09:19:01 +0000

    - fixed bug in 'resempls.R' - changed 'DESCRIPTION' (semPLS-dev)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sempls@90 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/semPLS/R/resempls.R
+++ pkg/semPLS/R/resempls.R
@@ -25,6 +25,7 @@ function(sempls, data, start=c("ones", "old"), method, ...){
     i <- sempls$iterations
     index <- sempls$weights_evolution$iteration==i
     weights_evolution <- sempls$weights_evolution[index,]
+    Hanafi <- subset(sempls$Hanafi, subset=(iterarion==i))
   }
   else if(start=="ones"){
     # Weights not adding up to 1 (14.08.2009)
@@ -40,6 +41,7 @@ function(sempls, data, start=c("ones", "old"), method, ...){
     else Wold <- stp1$outerW
     index <- sempls$weights_evolution$iteration==0
     weights_evolution <- sempls$weights_evolution[index,]
+    Hanafi <- subset(sempls$Hanafi, subset=(iterarion==0))
   }
 
   #############################################
