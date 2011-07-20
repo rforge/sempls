dcd22e3fe439a7b2204f18b2a7b20519e6543f56
amonecke <amonecke@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-07-20 17:01:21 +0000

    - fixed bug in 'resempls.R'
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sempls@91 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/semPLS/R/resempls.R
+++ pkg/semPLS/R/resempls.R
@@ -25,7 +25,7 @@ function(sempls, data, start=c("ones", "old"), method, ...){
     i <- sempls$iterations
     index <- sempls$weights_evolution$iteration==i
     weights_evolution <- sempls$weights_evolution[index,]
-    Hanafi <- subset(sempls$Hanafi, subset=(iterarion==i))
+    Hanafi <- sempls$Hanafi[i+1,] # Values of the last iteration
   }
   else if(start=="ones"){
     # Weights not adding up to 1 (14.08.2009)
@@ -41,7 +41,7 @@ function(sempls, data, start=c("ones", "old"), method, ...){
     else Wold <- stp1$outerW
     index <- sempls$weights_evolution$iteration==0
     weights_evolution <- sempls$weights_evolution[index,]
-    Hanafi <- subset(sempls$Hanafi, subset=(iterarion==0))
+    Hanafi <- sempls$Hanafi[1,] # iteration==0
   }
 
   #############################################
